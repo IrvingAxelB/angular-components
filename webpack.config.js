cae33bf117e5797638911b3ed853bf273747a044
Irving <Irvingb232@gmail.com>
2016-03-16 17:32:13 -0700

    [Adds] webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/*
+config for webpack. Will be used in
+the Gulpfile for building our app.
+Does not need gulp in order to do so,
+but we use gulp to orchestrate
+ */
+module.exports = {
+  output: {
+    filename: 'bundle.js'
+  },
+
+  devtool: 'sourcemap',
+
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.styl$/, loader: 'css!style!stylus' },
+      { test: /\.js$/, loader: 'babel?stage=1', exclude: [/node_modules/] }
+    ]
+  },
+
+  stylus: {
+    use: [require('jeet')(), require('rupture')()]
+  }
+};
